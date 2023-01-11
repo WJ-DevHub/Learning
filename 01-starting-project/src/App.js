2c962ecea9455e260df64e475fbe2b6ad0d0fd19
WJ-DevHub <weijie.fong@foodpanda.com>
2023-01-11 15:49:51 +0800

    Initial commit

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -5,11 +5,14 @@ import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+  
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn','1')
     setIsLoggedIn(true);
   };
 
