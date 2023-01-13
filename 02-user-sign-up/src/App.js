84a7d991e073d19817342fcb4d025afaf7b245d7
WJ-DevHub <weijie.fong@foodpanda.com>
2023-01-13 15:47:31 +0800

    initial commit

---
+++ 02-user-sign-up/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AddUser from "./components/Users/AddUser";
+
+function App(){
+  return (
+    <React.Fragment>
+    <div>
+      <AddUser />
+    </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
